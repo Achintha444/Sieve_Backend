b54616283a4a1847c2ba8716762c347845c9ff18
Achinths Isuru <achinthaisuru444@gmail.com>
2020-04-15 01:00:47 +0530

    app front end is connected

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
+const hashFunctions = require('../utils/hash_function');
 
-exports.user_login = (req, res, next) => {
+exports.user_login2 = (req, res, next) => {
     const email = 'temp123@gmail.com';//req.body.email;
     const password = 'sieve';//req.body.password
     User.getUserFromEmail(email).then((user) => {
@@ -8,7 +9,33 @@ exports.user_login = (req, res, next) => {
             console.log(user);
             res.json({
                 email: user.email,
+                password : password
             });
+        }
+    }).catch((err) => {
+        if (err) {
+            res.status(404).json({ error: 'Database Connection Faliure!' });
+        }
+    });
+}
+
+exports.user_login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password
+    User.getUserFromEmail(email).then((user) => {
+        if (user) {
+            if (hashFunctions.checkHash(password, user.password)) {
+                console.log(user);
+                res.json({
+                    email: user.email,
+                    password: password
+                });
+            }
+            else {
+                res.status(404).json({ error: 'Wrong Password' });
+            }
+        } else{
+            res.status(404).json({ error: 'Wrong Password' });
         }
     }).catch((err) => {
         if (err) {
--- utils/hash_function.js
+++ utils/hash_function.js
@@ -8,3 +8,12 @@ exports.encrypt = (password)=> {
 exports.decrypt = (hashPassowrd) => {
     return cryptr.decrypt(hashPassowrd);
 }
+
+exports.checkHash = (password, hashPassowrd) => {
+    var tPassword = cryptr.decrypt(hashPassowrd);
+    if(password==tPassword){
+        return true;
+    }else{
+        return false;
+    }
+}
