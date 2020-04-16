ee46cfef14b4f42e40a95b7a9e0c88366c2c863c
Achinths Isuru <achinthaisuru444@gmail.com>
2020-04-17 00:34:12 +0530

    privacy_tips routes added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRoute');
+var privacyTipsRoute = require('./routes/privacyTipsRoute');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
+app.use('/privacyTips',privacyTipsRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
